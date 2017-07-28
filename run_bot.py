6befdd83feee54d7ffa38fcb7faa8b0bc3e96293
rodrigo <rodrigo@bla.com>
2017-07-28 16:36:32 -0300

    teste auto

---
+++ run_bot.py
@@ -0,0 +1,23 @@
+import datetime
+import time
+
+def check_time():
+	data = datetime.datetime.now()
+	semana_atual = data.isocalendar()[1]
+	return data, semana_atual
+
+def main():
+	init = check_time()
+	a = check_time()
+	while True:
+		b = check_time()
+		c = b[0]-a[0]
+		if c.total_seconds() > 1:
+			a = check_time()
+			k = b[0]-init[0]
+			print k.total_seconds()
+			if k.total_seconds() > 10:
+				break
+
+if __name__ == "__main__":
+	main()
---
+++ teste_auto.py
@@ -0,0 +1,9 @@
+import os
+
+def main():
+	while True:
+		os.system("python run_bot.py")
+		print '\n\nde novo de novo\n\n'
+
+if __name__ == "__main__":
+	main()
