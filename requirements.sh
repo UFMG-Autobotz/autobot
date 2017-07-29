6b1aecef0c5c9ef0c124a5f00826c5cf0c11c6e3
Rodrigo Cézar <rodrigocezarluz@gmail.com>
2017-07-28 22:05:27 -0300

    add requirements

---
+++ requirements.sh
@@ -0,0 +1,10 @@
+apt install texlive-latex-base
+apt install texlive-fonts-recommended
+apt install texlive-fonts-extra
+apt install texlive-latex-extra
+apt install texlive-lang-portuguese
+pip install requests
+pip install pyyaml
+pip install slackclient
+# pip install pygithub
+pip install gitpython
